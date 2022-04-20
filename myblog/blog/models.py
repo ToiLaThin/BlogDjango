83f9c950e52fed6e9c07c97a8ff4c8ed0324eac3
Nguyen Duc Thinh <20110729@student.hcmute.edu.vn>
2022-04-20 21:41:12 +0700

    Rich text editor

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 import datetime
 
 # Create your models here.
@@ -11,15 +12,16 @@ class Post(models.Model):
     title = models.CharField(max_length=300)
     title_tag = models.CharField(max_length=300, default="Default title tag")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField()
     pub_date = models.DateField(default=timezone.now)
-    category = models.CharField(max_length=300,default='nothing')
+    category = models.CharField(max_length=300, default='nothing')
 
     def __str__(self):
         return self.title + " | " + str(self.author)
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
 
 class Category(models.Model):
     name = models.CharField(max_length=300)
